c97095388c75f0a2f05ce929ce26cf0a3d4d7635
Bunty Kumar <92105094+bunty187@users.noreply.github.com>
2024-08-04 02:46:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from peft import PeftModel, PeftConfig
-from transformers import GenerationConfig
+mport json
 import os
+from pprint import pprint
+
+import bitsandbytes as bnb
+import pandas as pd
+import torch
+import torch.nn as nn
+import transformers
+from datasets import load_dataset
+# from huggingface_hub import notebook_login
+from peft import (
+    LoraConfig,
+    PeftConfig,
+    PeftModel,
+    get_peft_model,
+    prepare_model_for_kbit_training,
+)
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+)
 
 # Configuration for PEFT model
 # os.getenv["HUGGING_FACE_TOKEN"]=""
